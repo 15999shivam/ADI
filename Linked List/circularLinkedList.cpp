039b00ea9de39c9d51b018cfec11fe9b346a970e
Shivam <shivamgarg260@gmail.com>
2019-10-14 00:00:23 +0530

    created circular linked list

---
+++ Linked List/circularLinkedList.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <stddef.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node *prev;
+};
+
+class List
+{
+private:
+    node *tail;
+
+public:
+    List()
+    {
+        tail = NULL;
+    }
+
+    //print the list
+    void display()
+    {
+        node *temp = tail;
+        while (temp->next != tail)
+        {
+            cout << temp->data << "\t";
+            temp = temp->next;
+        }
+        cout << temp->data << "\t";
+    }
+
+    //insert functions
+
+    //insert at begining
+    void insert_start(int value)
+    {
+        node *temp = new node;
+        temp->data = value;
+        if (tail != NULL)
+        {
+            temp->next = tail->next;
+            tail->next = temp;
+            temp->prev = tail;
+        }
+        else
+        {
+            tail = temp;
+            tail->next = tail;
+            tail->prev = tail;
+        }
+    }
+
+    //insert at last
+    void insert_last(int value)
+    {
+        node *temp = new node;
+        if (tail == NULL)
+        {
+            this->insert_start(value);
+        }
+        else
+        {
+            temp->data = value;
+            temp->next = tail->next;
+            temp->prev = tail;
+            tail->next = temp;
+            tail = temp;
+        }
+
+        // temp->next = NULL;
+        // if (head == NULL)
+        // {
+        //     head = temp;
+        //     temp->prev = NULL;
+        //     tail = temp;
+        // }
+        // else
+        // {
+        //     tail->next = temp;
+        //     temp->prev = tail;
+        //     tail = temp;
+        // }
+    }
+
+    //insert node at a position i
+    void insert_position(int pos, int value)
+    {
+        if (pos == 0)
+        {
+            this->insert_start(value);
+        }
+        else
+        {
+            node *pre = NULL;
+            node *cur = NULL;
+            node *temp = NULL;
+            cur = tail->next;
+            for (int i = 1; i < pos; i++)
+            {
+                pre = cur;
+                cur = cur->next;
+            }
+            temp->data = value;
+            pre->next = temp;
+            temp->prev = pre;
+            temp->next = cur;
+        }
+    }
+
+    //delete functions
+
+    //delete first node
+    void delete_first()
+    {
+        node *temp;
+        temp = tail->next;
+        if (team->next != NULL)
+            tail->next = tail->next->next;
+        delete temp;
+    }
+
+    //delete last node
+    void delete_last()
+    {
+        node *current = NULL;
+        node *previous = NULL;
+        current = tail->next;
+        if (head != NULL)
+        {
+            while (current->next != NULL)
+            {
+                previous = current;
+                current = current->next;
+            }
+            tail = previous;
+            delete current;
+            if (tail != NULL)
+                previous->next = NULL;
+        }
+    }
+
+    //delete at position i
+    void delete_position(int pos)
+    {
+        node *current = new node;
+        node *previous = new node;
+        current = head;
+        for (int i = 1; i < pos; i++)
+        {
+            previous = current;
+            current = current->next;
+        }
+        previous->next = current->next;
+        current->next->prev = previous;
+    }
+};
+
+int main()
+{
+
+    List list;
+    //insert last
+    list.insert_start(1);
+    list.insert_last(2);
+    cout << "insert last" << endl;
+    list.display();
+    cout << endl
+         << endl;
+
+    //insert start
+    list.insert_start(0);
+    cout << "insert start" << endl;
+    list.display();
+    cout << endl
+         << endl;
+
+    //insert index i
+    list.insert_position(2, 5);
+    cout << "insert at position 2" << endl;
+    list.display();
+    cout << endl
+         << endl;
+
+    //delete from end
+    list.delete_last();
+    cout << "delete last" << endl;
+    list.display();
+    cout << endl
+         << endl;
+
+    //delete first
+    list.delete_first();
+    cout << "delete first" << endl;
+    list.display();
+    cout << endl
+         << endl;
+
+    //insert start
+    list.delete_position(2);
+    cout << "delete postion 2" << endl;
+    list.display();
+    cout << endl
+         << endl;
+
+    return 0;
+}
+
+//TODO: check null conditions on insert functions and also put some check conditions on insert and delete from given postion(1 position is not working).
